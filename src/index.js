c001a0bf837ac70e1937c2ba61e6a0402ef7b53a
Steven Conaway <steven.conaway@icloud.com>
2020-06-30 17:03:49 -0700

    Move css into /src

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import './css/default.css';
+import './css/layout.css';
+import './css/media-queries.css';
+
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
