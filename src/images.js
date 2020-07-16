93aa9601f73123db71c61f02c76d292af1aa3b4d
Steven Conaway <steven.conaway@icloud.com>
2020-07-16 11:55:26 -0700

    Actually, just use a lower quality

--- src/images.js
+++ src/images.js
@@ -3,7 +3,7 @@ const sharp = require('sharp');
 const resumeData = require('./resumeData.json');
 const projects = resumeData.portfolio.projects;
 
-const imageSizes = [300, 500, 600, 900, 1200];
+// const imageSizes = [300, 500, 600, 900, 1200];
 
 projects.forEach((project) => {
     if (!project.image.local) return;
@@ -12,31 +12,35 @@ projects.forEach((project) => {
         `assets/images/portfolio/${project.image.name}/original.${project.image.extension}`,
     );
 
-    imageSizes.forEach((size) => {
-        image
-            .resize(size)
-            .toFile(
-                `assets/images/portfolio/${project.image.name}/${project.image.name}-${size}.png`,
-                function (err) {
-                    err != null && console.log(err);
-                },
-            );
-        // .toFile(
-        //     `assets/images/portfolio/${project.image.name}/${project.image.name}-${size}.webp`,
-        //     function (err) {
-        //         err != null && console.log(err);
-        //     },
-        // );
-    });
+    // imageSizes.forEach((size) => {
+    //     image
+    //         .resize(size)
+    //         .toFile(
+    //             `assets/images/portfolio/${project.image.name}/${project.image.name}-${size}.png`,
+    //             function (err) {
+    //                 err != null && console.log(err);
+    //             },
+    //         );
+    //     // .toFile(
+    //     //     `assets/images/portfolio/${project.image.name}/${project.image.name}-${size}.webp`,
+    //     //     function (err) {
+    //     //         err != null && console.log(err);
+    //     //     },
+    //     // );
+    // });
 
-    image.toFile(
-        `assets/images/portfolio/${project.image.name}/${project.image.name}-full.png`,
-        function (err) {
-            err != null && console.log(err);
-        },
-    );
+    image
+        .jpeg({
+            quality: 30,
+        })
+        .toFile(
+            `assets/images/portfolio/${project.image.name}/${project.image.name}.jpeg`,
+            function (err) {
+                err != null && console.log(err);
+            },
+        );
     // .toFile(
-    //     `assets/images/portfolio/${project.image.name}/${project.image.name}-full.webp`,
+    //     `assets/images/portfolio/${project.image.name}/${project.image.name}.webp`,
     //     function (err) {
     //         err != null && console.log(err);
     //     },
