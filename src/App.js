f8afeadd1adcfd2eb4dd8fe47d0d87bb5afb06a0
Steven Conaway <steven.conaway@icloud.com>
2020-07-28 20:43:42 -0700

    Explictly import needed icons

--- src/App.js
+++ src/App.js
@@ -8,11 +8,26 @@ import Contact from './Containers/Contact/Contact';
 import Portfolio from './Containers/Portfolio/Portfolio';
 
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {fab} from '@fortawesome/free-brands-svg-icons';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {far} from '@fortawesome/free-regular-svg-icons';
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import {
+    faEnvelope,
+    faAngleDown,
+    faAngleUp,
+    faDownload,
+    faLink,
+    faCheck,
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab, fas, far);
+library.add(
+    faLinkedin,
+    faGithub,
+    faEnvelope,
+    faAngleDown,
+    faAngleUp,
+    faDownload,
+    faLink,
+    faCheck,
+);
 
 const App = () => {
     return (
