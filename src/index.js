ad1824549bf7fb05515bd9131210dd9eb06131bf
Steven Conaway <steven.conaway@icloud.com>
2020-10-23 16:18:15 -0700

    Pre-upgrade changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
-// import {render} from 'react-snapshot';
-import {render} from 'react-dom';
-
+import ReactDOM from 'react-dom';
 import './index.css';
-import './css/default.css';
-import './css/layout.css';
-import './css/media-queries.css';
-
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import * as serviceWorker from './serviceWorker';
-
-render(
+ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
     document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
