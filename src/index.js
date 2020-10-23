15a2df2e7e1056f867578716f3bf83804a0db001
Steven Conaway <steven.conaway@icloud.com>
2020-10-23 16:32:34 -0700

    Upgrade to react ^17.0.1, react-scripts ^4.0.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import './css/default.css';
+import './css/layout.css';
+import './css/media-queries.css';
 
 ReactDOM.render(
     <React.StrictMode>
