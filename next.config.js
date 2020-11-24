e225cf0851a7355ce90a105244668644e8abbb76
Steven Conaway <steven.conaway@icloud.com>
2020-11-23 20:22:24 -0800

    Remove @zeit/next-css

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 // next.config.js
 
-const withCSS = require('@zeit/next-css');
+// const withCSS = require('@zeit/next-css');
 
 module.exports = {
-    ...withCSS({
-        /* config options here */
-    }),
+    // ...withCSS({
+    //     /* config options here */
+    // }),
     reactStrictMode: true,
     devIndicators: {
         autoPrerender: true,
