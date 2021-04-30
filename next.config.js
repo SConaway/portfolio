3ad1004595060418a8a01f266bae083c6b3e28d3
Steven Conaway <steven.conaway@icloud.com>
2021-04-29 21:22:59 -0700

    Remove old next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 // next.config.js
 
-// const withCSS = require('@zeit/next-css');
-
 module.exports = {
-    // ...withCSS({
-    //     /* config options here */
-    // }),
     reactStrictMode: true,
-    devIndicators: {
-        autoPrerender: true,
-    },
 };
