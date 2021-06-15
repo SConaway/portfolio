f11a0d132d6f21bdf2ff319099a457690e0192c6
Steven Conaway <steven.conaway@icloud.com>
2021-06-15 13:46:26 -0700

    Defer jQuery, waypoints, and custom scripts
    They're not critical to the first paint or anything, and only
    exist for the look-and-feel, so it's fine if they're not immediately
    present like the content.

--- pages/index.js
+++ pages/index.js
@@ -28,15 +28,17 @@ export default function Home() {
                 src='https://code.jquery.com/jquery-3.6.0.min.js'
                 integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
                 crossOrigin='anonymous'
+                defer
             ></script>
 
             <script
                 src='https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js'
                 integrity='sha256-w49DIDk8itgSjjuoof5lTxYKwcjWJ7Z2sRW3nU1StNA='
                 crossOrigin='anonymous'
+                defer
             ></script>
 
-            <script src='/init.js'></script>
+            <script src='/init.js' defer></script>
         </>
     );
 }
